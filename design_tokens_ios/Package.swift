56a07c1e0115b23f9c72730b39ddacfca1be04f3
si.ma <si.ma@fresha.com>
2022-11-15 14:42:35 +0000

    Test

--- design_tokens_ios/Package.swift
+++ design_tokens_ios/Package.swift
@@ -21,7 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "design_tokens_ios",
-            dependencies: []),
-        .binaryTarget(name: "DesignTokens", path: "./DesignTokens.xcframework")
+            dependencies: [])
     ]
 )
